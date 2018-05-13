7fcc52fec964f2919c1d3f2919720d211e30f32f
caian <caian@jd.com>
2018-05-13 11:10:07 +0800

    1.测试git提交方式

---
+++ steam-utils/src/main/java/com/an/steam/App.java
@@ -0,0 +1,18 @@
+package com.an.steam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+
+
+        List<String>  list = new ArrayList<>();
+        list.add("111");
+        System.out.println(list);
+        System.out.println("Hello World!");
+    }
+}
