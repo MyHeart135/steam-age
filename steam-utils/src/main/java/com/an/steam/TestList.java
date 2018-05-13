fd4fef3b1d135b6dc04cb41678d924b5d37f07a3
caian <caian@jd.com>
2018-05-13 11:12:40 +0800

    1.增加测试

---
+++ steam-utils/src/main/java/com/an/steam/TestList.java
@@ -0,0 +1,21 @@
+package com.an.steam;
+
+/**
+ * Created with IDEA
+ * Author: caian
+ * Modify:caian
+ * Description
+ * Date:2018/5/13
+ * Time:11:11
+ */
+public class TestList {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
